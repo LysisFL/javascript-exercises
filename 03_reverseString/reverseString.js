c90c630dc873df5de3f03a4079ad57d6558c85fd
LysisFL <lysisfl@gmail.com>
2024-08-29 20:08:01 +0200

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    const textArray = text.split("")
+    let result = ""
+    for (let i = textArray.length -1 ; i>=0 ; i--) 
+        result = result.concat(textArray[i])
+    return result
 };
 
 // Do not edit below this line
