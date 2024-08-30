b7c0c73debacef71726a9a70de1c068fa7250eec
LysisFL <lysisfl@gmail.com>
2024-08-30 12:59:26 +0200

    Complete Exercise 3

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(arr, ...numbers) {
+    const argsArray = Array.from(numbers)
+    const finalArray = []
+    let removeFromArray
+
+    for (let arrNum of arr) {
+      removeFromArray = false
+      for (let argNum of argsArray) {
+          if (argNum === arrNum) {
+            removeFromArray = true;
+            break;
+          }
+      }
+      if(!removeFromArray) {
+        finalArray.push(arrNum)
+      }
+    }
+  
+    return finalArray;
+  } 
 
 // Do not edit below this line
 module.exports = removeFromArray;
