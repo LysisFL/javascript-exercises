20aa457ecb5e0abf92e1b6fe942710f582fec0e1
LysisFL <lysisfl@gmail.com>
2024-08-30 13:13:44 +0200

    Complete Exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (num1<0 || num2<0 || !Number.isInteger(num1) || !Number.isInteger(num2))
+        return "ERROR"
 
+    let sum = 0;
+    let smallNumber
+    let largeNumber
+    if(num1 < num2) {
+        smallNumber = num1
+        largeNumber = num2
+    } else {
+        smallNumber = num2
+        largeNumber = num1
+    }
+    for(smallNumber;smallNumber<=largeNumber;smallNumber++) {
+        sum += smallNumber;
+    }
+    return sum;
 };
 
 // Do not edit below this line
